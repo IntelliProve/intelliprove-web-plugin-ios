6acd37abc6e5f479bebe9a08d6b226c93ff2fe40
Dries Vanmeert <dries.vanmeert@gmail.com>
2023-12-22 22:32:40 +0100

    Initial Commit

---
+++ IntelliProve POC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntelliProve POC
+//
+//  Created by Dries Vanmeert on 22/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntelliProve POC/IntelliProve_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelliProve_POCApp.swift
+//  IntelliProve POC
+//
+//  Created by Dries Vanmeert on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct IntelliProve_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
