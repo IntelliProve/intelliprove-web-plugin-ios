a182a4cbd3ea8eb354f2416280f409dd5af3bee0
Dries Vanmeert <dries.vanmeert@gmail.com>
2023-12-26 18:30:15 +0100

    Set up of mini SwiftUI project with a WKWebView * Need to set Info.plist values for Camera/Microphone usage to get permission * To play inline, WKWebViewConfiguration has to set `allowsInlineMediaPlayback` and `mediaTypesRequiringUserActionForPlayback` BEFORE init of WKWebView * Then the application simply runs * Add Readme file

--- IntelliProve POC/ContentView.swift
+++ IntelliProve POC/ContentView.swift
@@ -6,16 +6,58 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    let url = "https://plugin-dev.intelliprove.com/?action_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiIiwiY3VzdG9tZXIiOiJOZWJ1bGFlIHRlc3RpbmciLCJncm91cCI6ImFkbWluIiwibWF4X21lYXN1cmVtZW50X2NvdW50IjoxMDAwfSwibWV0YSI6e30sImV4cCI6MTcxNTA3NDIyMn0.kQvGQD_8wFzmLjgFMuft_i3nWjAxSKWx5oI_FBFEYXI"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        WebView(url: url).edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        // Ensure inline camera access
+        // Important to set this configuration *before* starting the WKWebView
+        // Otherwise the camera is opened fullscreen anyway, instead of under the custom UI
+        let configuration = WKWebViewConfiguration()
+        configuration.mediaTypesRequiringUserActionForPlayback = []
+        configuration.allowsInlineMediaPlayback = true
+
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = context.coordinator
+        webView.uiDelegate = context.coordinator
+        webView.contentMode = .scaleToFill
+
+        // Needed to inspect the Web Session from within Safari Dev Tools
+        webView.isInspectable = true
+
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: url) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+            uiView.contentMode = .scaleToFill
         }
-        .padding()
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
+        var parent: WebView
+
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+
+        // Implement WKNavigationDelegate and WKUIDelegate methods if needed
     }
 }
 
